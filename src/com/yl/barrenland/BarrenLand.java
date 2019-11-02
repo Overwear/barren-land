63c0676bfd4364f7abe5874b5f86b4f90bb8ddc5
overwear <lee48493@stthomas.edu>
2019-11-01 21:39:12 -0500

    Add BFS alg

--- src/com/yl/barrenland/BarrenLand.java
+++ src/com/yl/barrenland/BarrenLand.java
@@ -1,48 +1,116 @@
 package com.yl.barrenland;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BarrenLand {
     final static int X_LIMIT = 400;
     final static int Y_LIMIT = 600;
-    List<Integer[]> barrenLandCoordinates = new ArrayList<>();
+    List<int[]> barrenLandCoordinates = new ArrayList<int[]>();
+    Queue<int[]> queue = new LinkedList<>();
+    Map<Integer, Integer> areaMap = new HashMap<>();
     int[][] grid = new int[X_LIMIT][Y_LIMIT];
+
+    public BarrenLand() {
+        // constructor
+    }
 
     // read input
     void parseInput(String in) {
         String[] subStrings = in.split(",");
         for (String s : subStrings) {
             s = s.replaceAll("\"", "");
-            s = s.replaceAll("{", "");
-            s = s.replaceAll("}", "");
+            s = s.replaceAll("\\{", "");
+            s = s.replaceAll("\\}", "");
+            s = s.replaceAll("^ ", "");
 
             String[] coordinates = s.split(" ");
             int x1 = Integer.parseInt(coordinates[0]);
             int y1 = Integer.parseInt(coordinates[1]);
             int x2 = Integer.parseInt(coordinates[2]);
             int y2 = Integer.parseInt(coordinates[3]);
-
+            int[] rectangle = {x1, y1, x2, y2};
+            barrenLandCoordinates.add(rectangle);
         }
     }
 
-}
-    // parse input string to int
+    void printCoordinates() {
+        for (int[] i : barrenLandCoordinates) {
+            System.out.println(Arrays.toString(i));
+        }
+    }
 
-    // store coordinates in a datastructure
-
+    // initialize grid to 0
+    void initGrid() {
+        for (int[] row : grid) {
+            Arrays.fill(row, 0);
+        }
+    }
 
     // fill in barren land function
+    void fillBarrenLand() {
+        for (int[] coordinates : barrenLandCoordinates) {
+            int x1 = coordinates[0];
+            int y1 = coordinates[1];
+            int x2 = coordinates[2];
+            int y2 = coordinates[3];
+
+            for (int i = x1; i < x2; i++) {
+                for (int j = y1; j < y2; j++) {
+                    grid[i][j] = 1;     // 1 means barren land
+                }
+            }
+        }
+    }
 
     // create ds that stores disconnected areas
 
     // function that counts the disconnected areas
+    void countFertileLand() {
+        int landNumber = 1;     // 2 represents the first fertile land
+        int counter = 0;
+        for (int i = 0; i < X_LIMIT; i++) {
+            for (int j = 0; j < Y_LIMIT; j++) {
+                if (grid[i][j] == 0) {
+                    counter = 1;
+                    queue.add(new int[]{i, j});
+                    landNumber++;
+                } else {
+                    continue;
+                }
+                while (!queue.isEmpty()) {
+                    int[] coordinate = queue.remove();
+                    int x = coordinate[0];
+                    int y = coordinate[1];
 
-    // printing each area in sorted order
+                    // Set the cell to a land area number if fertile
+                    if (grid[x][y] == 0) {
+                        grid[x][y] = landNumber;
+                        counter++;
 
-    //
-
-    public static void main(String[] args) {
-
+                        // Add the cell above
+                        if (y < Y_LIMIT - 1) {
+                            queue.add(new int[]{x, y + 1});
+                        }
+                        // Add the cell below
+                        if (y > 0) {
+                            queue.add(new int[]{x, y - 1});
+                        }
+                        // Add the cell to the right
+                        if (x < X_LIMIT - 1) {
+                            queue.add(new int[]{x + 1, y});
+                        }
+                        // Add the cell to the left
+                        if (x > 0) {
+                            queue.add(new int[]{x - 1, y});
+                        }
+                    }
+                }
+                areaMap.put(landNumber, counter);
+            }
+        }
     }
+
+    // Print Fertile Land Area and Sort
+
+
 }
